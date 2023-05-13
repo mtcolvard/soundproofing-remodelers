b655da996b2cd2010d0af1cc72365dba42c0a0d0
Matty Talty <matthew.colvard@gmail.com>
2023-05-12 17:15:22 -0700

    feature and testimonial mockup

---
+++ app/components/feature-section.js
@@ -0,0 +1,64 @@
+import { HomeModernIcon, AtSymbolIcon, ChatBubbleLeftRightIcon, WrenchScrewdriverIcon, DocumentMagnifyingGlassIcon } from '@heroicons/react/20/solid'
+
+const features = [
+  {
+    name: 'Consultations',
+    description:
+      'On-site consultations to assess optimal soundproofing remedies to meet your personal and budget particular needs.',
+    href: '#',
+    icon: ChatBubbleLeftRightIcon,
+  },
+  {
+    name: 'Installation & Remodeling',
+    description:
+      'Extensive experience mitigating noise in single & multifamily homes, mixed-use developments, and commercial properties.',
+    href: '#',
+    icon: WrenchScrewdriverIcon,
+  },
+  {
+    name: 'Compliance Reports',
+    description:
+      'Acoustical testing, design services, and compliance reports to satisfy Title 25 and Section 1207 of the California State Building Code.',
+    href: '#',
+    icon: DocumentMagnifyingGlassIcon,
+  },
+]
+
+export default function FeatureSection() {
+  return (
+    <div className="bg-gray-900 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-400">We provide</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            everything you need to achieve peace of mind
+          </p>
+          {/* <p className="mt-6 text-lg leading-8 text-gray-300">
+            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
+            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+          </p> */}
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+            {features.map((feature) => (
+              <div key={feature.name} className="flex flex-col">
+                <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
+                  <feature.icon className="h-5 w-5 flex-none text-indigo-400" aria-hidden="true" />
+                  {feature.name}
+                </dt>
+                <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
+                  <p className="flex-auto">{feature.description}</p>
+                  <p className="mt-6">
+                    <a href={feature.href} className="text-sm font-semibold leading-6 text-indigo-400">
+                      Learn more <span aria-hidden="true">→</span>
+                    </a>
+                  </p>
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  )
+}
