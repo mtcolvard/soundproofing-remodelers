59879fef9937fd1ad0f67146171b69e03183f7d8
Matty Talty <matthew.colvard@gmail.com>
2023-05-17 03:08:53 -0700

    header layout with background image complete. sans and mono font families not working for some reason

--- app/components/feature-section.js
+++ app/components/feature-section.js
@@ -26,11 +26,11 @@ const features = [
 
 export default function FeatureSection() {
   return (
-    <div className="bg-gray-900 py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <div className="font-sans bg-zinc-50 py-24 sm:py-32">
+      <div className=" mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-400">We provide</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">We provide</h2>
+          <p className="mt-2 text-3xl font-sans font-bold tracking-tight text-gray-900 sm:text-4xl">
             everything you need to achieve peace of mind
           </p>
           {/* <p className="mt-6 text-lg leading-8 text-gray-300">
@@ -38,18 +38,21 @@ export default function FeatureSection() {
             pulvinar et feugiat blandit at. In mi viverra elit nunc.
           </p> */}
         </div>
+        
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
             {features.map((feature) => (
-              <div key={feature.name} className="flex flex-col">
-                <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                  <feature.icon className="h-7 w-7 flex-none text-indigo-400" aria-hidden="true" />
-                  {feature.name}
+              <div key={feature.name} className=" flex flex-col">
+                <dt className="flex items-center gap-x-3 text-lg font-semibold leading-7 text-gray-900">
+                  <div className="relative left-0 top-0 flex h-8 w-8 items-center justify-center rounded-lg bg-indigo-600">
+                  <feature.icon className="h-6 w-6 flex-none text-white " aria-hidden="true" />
+                  </div>
+                    {feature.name}
                 </dt>
-                <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
-                  <p className="flex-auto">{feature.description}</p>
+                <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                  <p className="flex-auto ">{feature.description}</p>
                   <p className="mt-6">
-                    <a href={feature.href} className="text-sm font-semibold leading-6 text-indigo-400">
+                    <a href={feature.href} className="text-sm font-semibold leading-6 text-indigo-600">
                       Learn more <span aria-hidden="true">→</span>
                     </a>
                   </p>
--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,34 @@
 import './globals.css'
-import { inter } from './fonts'
+import { Inter, Roboto_Mono, Ysabeau } from 'next/font/google';
 
 export const metadata = {
   title: 'Soundproofing Remodelers',
   description: 'Soundproofing Remodelers, Acoustical consultants to the Bay Area since 1998',
 }
 
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto-mono',
+});
+
+const ysabeau = Ysabeau({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-ysabeau',
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${inter.variable} ${roboto_mono.variable} ${ysabeau.variable}`}>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
